707d271d72923f715b7234dcba756f960f679118
Andreas Svanberg <andreas.svanberg@mensa.se>
2021-11-05 22:13:31 +0100

    Better handling of missing properties. Fixes #14
    Optional gets sent the empty value instead of null and other types error out if they're missing.

--- src/main/java/io/github/asvanberg/donkey/deserializing/ObjectDeserializer.java
+++ src/main/java/io/github/asvanberg/donkey/deserializing/ObjectDeserializer.java
@@ -1,11 +1,20 @@
 package io.github.asvanberg.donkey.deserializing;
 
+import io.github.asvanberg.donkey.exceptions.MissingPropertyInJsonException;
 import jakarta.json.bind.serializer.DeserializationContext;
 import jakarta.json.stream.JsonParser;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 
 class ObjectDeserializer<T> extends NullableDeserializer<T> {
+    private static final Object MISSING_FROM_JSON = new Object();
+
     private final Creator<T> creator;
 
     static <T> ObjectDeserializer<T> of(Class<T> clazz) {
@@ -20,6 +29,7 @@ class ObjectDeserializer<T> extends NullableDeserializer<T> {
     protected T getValue(final JsonParser parser, final DeserializationContext ctx) {
         Util.assertCurrentParserPosition(JsonParser.Event.START_OBJECT, parser);
         final Object[] creationParameters = new Object[creator.parameterCount()];
+        Arrays.fill(creationParameters, MISSING_FROM_JSON);
         while (parser.next() != JsonParser.Event.END_OBJECT) {
             final String parameterName = parser.getString();
             final Optional<Creator.Parameter> parameter = creator.getParameterByName(parameterName);
@@ -32,7 +42,37 @@ class ObjectDeserializer<T> extends NullableDeserializer<T> {
                 creationParameters[param.index()] = parameterValue;
             }
         }
+        for (Creator.Parameter parameter : creator.parameters()) {
+            if (creationParameters[parameter.index()] == MISSING_FROM_JSON) {
+                Object replacementValue = getReplacementValue(parameter.type());
+                if (replacementValue != null) {
+                    creationParameters[parameter.index()] = replacementValue;
+                }
+                else {
+                    throw new MissingPropertyInJsonException(parameter.name());
+                }
+            }
+        }
         return creator.create(creationParameters);
+    }
+
+    private Object getReplacementValue(final Type type)
+    {
+        if (type instanceof ParameterizedType pt && pt.getRawType() == Optional.class) {
+            return Optional.empty();
+        }
+        else if (type == OptionalInt.class) {
+            return OptionalInt.empty();
+        }
+        else if (type == OptionalLong.class) {
+            return OptionalLong.empty();
+        }
+        else if (type == OptionalDouble.class) {
+            return OptionalDouble.empty();
+        }
+        else {
+            return null;
+        }
     }
 
     private void skipAttribute(final JsonParser parser) {
