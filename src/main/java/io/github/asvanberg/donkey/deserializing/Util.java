109ece55080521340fb9652e13088199855f4bf4
Andreas Svanberg <andreas.svanberg@mensa.se>
2021-12-02 11:36:50 +0100

    Try canonical for records if no @JsonbCreator

--- src/main/java/io/github/asvanberg/donkey/deserializing/Util.java
+++ src/main/java/io/github/asvanberg/donkey/deserializing/Util.java
@@ -1,13 +1,26 @@
 package io.github.asvanberg.donkey.deserializing;
 
+import io.github.asvanberg.donkey.exceptions.InternalProcessingException;
 import io.github.asvanberg.donkey.exceptions.UnexpectedParserPositionException;
 import jakarta.json.stream.JsonParser;
+
+import java.util.concurrent.Callable;
 
 class Util {
     static void assertCurrentParserPosition(final JsonParser.Event expected, final JsonParser parser) {
         final JsonParser.Event event = parser.next();
         if (event != expected) {
             throw new UnexpectedParserPositionException(expected, event);
+        }
+    }
+
+    static <A> A throwing(Callable<A> f)
+    {
+        try {
+            return f.call();
+        }
+        catch (Exception e) {
+            throw new InternalProcessingException(e);
         }
     }
 }
